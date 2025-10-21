3ed0db38a0559c45fa27f34c6a6f2667273d64c3
Ivan BOLTENKOV <ivanwrest@gmail.com>
2025-10-21 23:28:05 +0300

    виправлення помилок

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,14 @@
-function getElementWidth(conten, padding, border) {
-    const contenn = Number.parseFloat(conten);
+function getElementWidth(content, padding, border) {
+    const contentn = Number.parseFloat(content);
     const paddingn = Number.parseFloat(padding);
     const bordern = Number.parseFloat(border);
 
-    const borderBox = contenn + (paddingn * 2) + (bordern * 2) ;
+    const borderBox = contentn + (paddingn * 2) + (bordern * 2) ;
 
-    return `${borderBox}`;
+    return borderBox;
     
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-    
